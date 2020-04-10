3262da0f058b865e79e6888afe9e1263125c7645
zjw <zjw@gimc.cn>
2020-04-10 11:23:34 +0800

    修改没有登录下前端报错的方法

--- demo-webapp-one/src/main/java/com/standard/demo/webapp/one/config/shiro/AjaxPermissionsAuthorizationFilter.java
+++ demo-webapp-one/src/main/java/com/standard/demo/webapp/one/config/shiro/AjaxPermissionsAuthorizationFilter.java
@@ -2,6 +2,7 @@ package com.standard.demo.webapp.one.config.shiro;
 
 import com.alibaba.fastjson.JSONObject;
 import com.standard.demo.web.core.constant.ErrorEnum;
+import com.standard.demo.web.core.utils.CommonUtil;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -20,16 +21,13 @@ public class AjaxPermissionsAuthorizationFilter extends FormAuthenticationFilter
 
 	@Override
 	protected boolean onAccessDenied(ServletRequest request, ServletResponse response) {
-		JSONObject jsonObject = new JSONObject();
-		jsonObject.put("code", ErrorEnum.E_20011.getErrorCode());
-		jsonObject.put("msg", ErrorEnum.E_20011.getErrorMsg());
 		PrintWriter out = null;
 		HttpServletResponse res = (HttpServletResponse) response;
 		try {
 			res.setCharacterEncoding("UTF-8");
 			res.setContentType("application/json");
 			out = response.getWriter();
-			out.println(jsonObject);
+			out.println(JSONObject.toJSONString(CommonUtil.errorJson(ErrorEnum.E_20011)));
 		} catch (Exception e) {
 		} finally {
 			if (null != out) {
